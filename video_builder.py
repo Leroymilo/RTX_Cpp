f924de2d2093851e86d5d035606877b6db1638f3
leroymilo <leroyhaveamailbox@gmail.com>
2022-07-21 22:28:15 +0200

    hopefully fixed video_builder

--- video_builder.py
+++ video_builder.py
@@ -1,79 +1,49 @@
-# -*- coding: utf-8 -*- 
-
-###########################################################################
-## Python code generated with wxFormBuilder (version Jun 17 2015)
-## http://www.wxformbuilder.org/
-##
-## PLEASE DO "NOT" EDIT THIS FILE!
-###########################################################################
-
 import wx
-import wx.xrc
+from video_builder_gen import Window
 
-###########################################################################
-## Class Window
-###########################################################################
+import imageio
+from PIL import Image
+import cv2
+from os import listdir
 
-class Window ( wx.Frame ):
-	
-	def __init__( self, parent ):
-		wx.Frame.__init__ ( self, parent, id = wx.ID_ANY, title = u"Video Builder", pos = wx.DefaultPosition, size = wx.Size( 300,110 ), style = wx.CAPTION|wx.CLOSE_BOX|wx.MINIMIZE_BOX|wx.TAB_TRAVERSAL )
-		
-		self.SetSizeHintsSz( wx.Size( 300,110 ), wx.DefaultSize )
-		
-		vSizer = wx.BoxSizer( wx.VERTICAL )
-		
-		hSizer_name = wx.BoxSizer( wx.HORIZONTAL )
-		
-		self.text_name = wx.StaticText( self, wx.ID_ANY, u"Name of result :", wx.DefaultPosition, wx.DefaultSize, 0 )
-		self.text_name.Wrap( -1 )
-		hSizer_name.Add( self.text_name, 0, wx.ALL|wx.ALIGN_CENTER_VERTICAL, 5 )
-		
-		self.textCtrl_name = wx.TextCtrl( self, wx.ID_ANY, u"result", wx.DefaultPosition, wx.DefaultSize, wx.TE_NO_VSCROLL|wx.TE_RIGHT )
-		hSizer_name.Add( self.textCtrl_name, 0, wx.ALL|wx.ALIGN_CENTER_VERTICAL, 5 )
-		
-		choice_extChoices = [ u".gif", u".avi", u".mp4" ]
-		self.choice_ext = wx.Choice( self, wx.ID_ANY, wx.DefaultPosition, wx.DefaultSize, choice_extChoices, 0 )
-		self.choice_ext.SetSelection( 0 )
-		hSizer_name.Add( self.choice_ext, 0, wx.ALL, 5 )
-		
-		
-		vSizer.Add( hSizer_name, 0, 0, 5 )
-		
-		hSizer_fps = wx.BoxSizer( wx.HORIZONTAL )
-		
-		self.text_pre_fps = wx.StaticText( self, wx.ID_ANY, u"Frame rate :", wx.DefaultPosition, wx.DefaultSize, 0 )
-		self.text_pre_fps.Wrap( -1 )
-		hSizer_fps.Add( self.text_pre_fps, 0, wx.ALL|wx.ALIGN_CENTER_VERTICAL, 5 )
-		
-		self.spinCtrl_fps = wx.SpinCtrl( self, wx.ID_ANY, wx.EmptyString, wx.DefaultPosition, wx.Size( 70,-1 ), wx.SP_ARROW_KEYS|wx.SP_WRAP, 1, 60, 15 )
-		hSizer_fps.Add( self.spinCtrl_fps, 0, wx.ALL|wx.ALIGN_CENTER_VERTICAL, 5 )
-		
-		self.text_post_fps = wx.StaticText( self, wx.ID_ANY, u"fps", wx.DefaultPosition, wx.DefaultSize, 0 )
-		self.text_post_fps.Wrap( -1 )
-		hSizer_fps.Add( self.text_post_fps, 0, wx.ALL|wx.ALIGN_CENTER_VERTICAL, 5 )
-		
-		self.button = wx.Button( self, wx.ID_ANY, u"Build video", wx.DefaultPosition, wx.DefaultSize, 0 )
-		hSizer_fps.Add( self.button, 0, wx.ALIGN_CENTER_VERTICAL|wx.ALL, 5 )
-		
-		
-		vSizer.Add( hSizer_fps, 0, 0, 5 )
-		
-		
-		self.SetSizer( vSizer )
-		self.Layout()
-		
-		self.Centre( wx.BOTH )
-		
-		# Connect Events
-		self.button.Bind( wx.EVT_BUTTON, self.build )
-	
-	def __del__( self ):
-		pass
-	
-	
-	# Virtual event handlers, overide them in your derived class
-	def build( self, event ):
-		event.Skip()
-	
+class VideoBuilder(Window) :
+    def __init__(self) :
+        super().__init__(None)
+    
+    def build(self, event) :
+        self.button.Disable()
+        if self.textCtrl_name.GetValue().strip() == '' :
+            name = "result"
+        else :
+            name = self.textCtrl_name.GetLabel()
+        for pos_ext in self.choice_ext.GetStrings() :
+            name.removesuffix(pos_ext)
+        ext = self.choice_ext.GetStringSelection()
+        fps = self.spinCtrl_fps.GetValue()
 
+        frames = ["renders\\"+f for f in listdir("renders")]
+
+        if ext == ".gif" :
+            writer = imageio.get_writer(
+                name + ext,
+                duration = 1/fps    #Duration of a frame in seconds
+                )
+            
+            for frame in frames :
+                writer.append_data(imageio.imread(frame))
+            writer.close()
+        
+        else :
+            size = Image.open(frames[0]).size
+            writer = cv2.VideoWriter(name + ext, 0, fps, size)
+
+            for frame in frames :
+                writer.write(cv2.imread(frame))
+            cv2.destroyAllWindows()
+            writer.release()
+        self.button.Enable()
+
+app = wx.App(False)
+frame = VideoBuilder()
+frame.Show()
+app.MainLoop()
